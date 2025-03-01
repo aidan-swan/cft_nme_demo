dbef559249b7d369e0bd89ff5fa931f83a7ad7ea
CasperL1218 <cl2622@cornell.edu>
2025-03-01 18:44:32 -0500

    Solve FizzBuzz

--- basics.js
+++ basics.js
@@ -1,28 +1,34 @@
-console.log("Hello");
+// console.log("Hello");
 
-let name1 = "Casper";
+// let name1 = "Casper";
 
-var name2 = "outdated";
+// var name2 = "outdated";
 
-console.log(name1);
+// console.log(name1);
 
-const name3 = "what";
+// const name3 = "what";
 
-console.log(name2);
+// console.log(name2);
 
-// array
-let nums = [1, 2, 3, 4, 5];
+// // array
+// let nums = [1, 2, 3, 4, 5];
 
-let found = false;
+// let found = false;
 
-for (let i = 0; i < nums.length; i++) {
-  if (nums[i] == 3) {
-    console.log("We have found 6 at index" + i);
-    found = true;
-    break;
-  }
+// for (let i = 0; i < nums.length; i++) {
+//   if (nums[i] == 3) {
+//     console.log("We have found 6 at index" + i);
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (!found) {
+//   console.log("We didn't have 6");
+// }
+
+function score(points, name) {
+  return name + " scored " + points + " today!";
 }
 
-if (!found) {
-  console.log("We didn't have 6");
-}
+console.log(score(50, "Casper"));
