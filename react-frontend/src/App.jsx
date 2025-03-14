fd92d65a736b54dd6bf4c7c6c3a5a2412aca9261
Aidan Swan <aidanswan@dhcp-vl2041-8416.eduroam.cornell.edu>
2025-03-14 11:53:46 -0400

    Updates

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,13 +1,28 @@
 // import { useState } from 'react'
-import Header from "./Header.jsx"
-
+import Homepage from "./Homepage.jsx"
+import Profilepage from "./Profilepage.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Header />
+      <Router>
+        <>
+          <nav>
+            <Link to="/" className="mr-4">Home</Link>
+            <p>-</p>
+            <Link to="/profile">Check out our profiles here</Link>
+          </nav>
+
+         
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/profile" element={<Profilepage />} />
+          </Routes>
+        </>
+      </Router>
     </>
   )
 }
---
+++ react-frontend/src/Homepage.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header.jsx"
+
+function Homepage () {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Homepage
